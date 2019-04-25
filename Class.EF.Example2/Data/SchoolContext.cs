fd2f42545bc4769d43a6a3a7c56762d490496114
Joachim Francois <joachim.francois@howest.be>
2019-04-25 15:27:31 +0200

    Add up to step 12

---
+++ Class.EF.Example2/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Class.EF.Example2.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Class.EF.Example2.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ Class.EF.Example2/Entities/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class.EF.Example2.Entities
+{
+    public class Teacher
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal YearlyWage { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
