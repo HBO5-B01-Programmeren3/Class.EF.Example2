dfc99f0ae412dd94c26aa9afce9167a18b440306
Joachim Francois <joachim.francois@howest.be>
2019-05-02 11:57:59 +0200

    Update up to p257

--- Class.EF.Example2/Controllers/HomeController.cs
+++ Class.EF.Example2/Controllers/HomeController.cs
@@ -1,29 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Class.EF.Example2.Data;
+using Class.EF.Example2.Entities;
 using Class.EF.Example2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Class.EF.Example2.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext schoolContext;
+
+        public HomeController(SchoolContext context)
+        {
+            schoolContext = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
-        }
+            var vm = new HomeIndexVm();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            long teachId = 10;
+            vm.TeacherWithIdOne = schoolContext.Teachers.Find(teachId);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            vm.StudentsBornBefore2k = schoolContext.Students
+                                                   .Where(s => s.Birthdate.Year < 2000)
+                                                   .OrderBy(s => s.Name)
+                                                   .ToList();
+            vm.TotalScholarships = schoolContext.Students.Sum(s => s.Scholarship) ?? 0;
+
+            vm.ScholarShipStudentsWithInfo = schoolContext.Students
+                .Include(s => s.ContactInfo)
+                .Where(s => s.Scholarship != null)
+                .OrderByDescending(s => s.Scholarship)
+                .ThenBy(s => s.Name)
+                .ToList();
+
+            var allStudentCourses = schoolContext.Set<StudentCourse>()
+                .Include(sc => sc.Course)
+                .ThenInclude(c => c.Lecturer)
+                .Include(sc => sc.Student)
+                .ThenInclude(s => s.ContactInfo).ToList();
+
+            vm.Courses = allStudentCourses.Select(sc => sc.Course)
+                .Distinct()
+                .OrderByDescending(sc => sc.StudentCourses.Count);
+
+            return View(vm);
         }
     }
 }
---
+++ Class.EF.Example2/Models/HomeIndexVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Class.EF.Example2.Entities;
+
+namespace Class.EF.Example2.Models
+{
+    public class HomeIndexVm
+    {
+        public Teacher TeacherWithIdOne { get; set; }
+
+        public IEnumerable<Student> StudentsBornBefore2k { get; set; }
+
+        public decimal TotalScholarships { get; set; }
+
+        public IEnumerable<Student> ScholarShipStudentsWithInfo { get; set; }
+
+        public IEnumerable<Course> Courses { get; set; }
+
+    }
+}
